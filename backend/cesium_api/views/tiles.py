879b564538a1706c7f2282da98da8e8648cf5eb0
lyla shukur <lylashukur2@gmail.com>
2025-04-12 16:49:02 -0700

    revert tiles.py to main version

--- backend/cesium_api/views/tiles.py
+++ backend/cesium_api/views/tiles.py
@@ -1,27 +1,16 @@
 from os.path import exists, abspath
 from json import dumps
 from flask import Blueprint, current_app, send_file
-from flask_cors import cross_origin
 
 view_tiles = Blueprint("view_tiles", __name__)
 
-
 @view_tiles.route("/<int:zoom>/<int:x>/<int:y>.png")
-#@cross_origin(origins=["http://localhost:5173", "http://localhost:3000"])
-@cross_origin(origins='*')
 def get_tile(zoom: int, x: int, y: int):
-    
-    tiles_dir = getattr(current_app, "tiles_dir", "../geo-tiles/tiles")
-    tile_path = abspath(f"{tiles_dir}/{zoom}/{x}/{y}.png")
-    print(f"[TILE DEBUG] Tile path being served: {tile_path}")
-
-    #tile_path = abspath(f"{data_dir}/tiles/{zoom}/{x}/{y}.png")
-    #print(f"[TILE DEBUG] Tile path being served: {tile_path}")
-
-
+    data_dir = current_app.data_dir
+    tile_path = abspath(f"{data_dir}/tiles/{zoom}/{x}/{y}.png")
 
     try:
-        #exists(tile_path)
+        exists(tile_path)
         return send_file(tile_path)
     except FileNotFoundError:
         return {"err": f"No such tile for {x} by {y} at a zoom of {zoom}"}, 404
